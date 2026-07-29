3ac1db5f6d4e709beec4282bf35bf3fd8d00f059
okushnarev <styx.inc4@gmail.com>
2026-07-29 16:11:41 +0700

    Add docstring to `unproject_depth()`

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,16 @@ import numpy as np
 def unproject_depth(
         depth: np.ndarray,
         intrinsics: dict[str, float],
-        mask: np.ndarray| None = None,
+        mask: np.ndarray | None = None,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Converts depth pixels to 3D coordinates in the camera frame
+    :param depth: Depth map (H, W) in meters
+    :param intrinsics: Dict with camera params 'fx', 'fy', 'cx', 'cy' in pixels
+    :param mask: (default: None) Boolean array (H, W) selecting which pixels to unproject.
+        If None, unprojects whole image
+    :return: Tuple of (X, Y, Z) 1D arrays in meter
+    """
     image_h, image_w = depth.shape
     u_grid, v_grid = np.meshgrid(np.arange(image_w), np.arange(image_h))
 
