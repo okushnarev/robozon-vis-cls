ecdaaab6014f9bf07ec140502ffc020392cc8543
okushnarev <styx.inc4@gmail.com>
2026-08-02 01:09:44 +0700

    Add ReturnData dataclass as return type

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,8 +1,17 @@
-from typing import Any
+from dataclasses import dataclass
 
 import numpy as np
+@dataclass
+class ReturnData:
+    length: float
+    width: float
+    height: float
+    roundness: float
+    is_round: bool
+    center_xy: tuple[float, float]
+    class_name: str
 
 
 class BaseClassifier:
-    def step(self, rgbd_img: np.ndarray) -> list[dict[int, dict[str, Any]]]:
+    def step(self, rgbd_img: np.ndarray) -> dict[int, ReturnData]:
         raise NotImplementedError
