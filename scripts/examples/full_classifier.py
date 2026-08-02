0730411a9d46053032662f37bb50e0715ddcd40b
okushnarev <styx.inc4@gmail.com>
2026-08-03 01:59:08 +0700

    Update examples to save video

--- scripts/examples/full_classifier.py
+++ scripts/examples/full_classifier.py
@@ -2,8 +2,11 @@ import json
 import sys
 from pathlib import Path
 
+import cv2
 import h5py
 import numpy as np
+import supervision as sv
+from tqdm import tqdm
 
 # Add project root to PATH
 if (project_root := str(Path.cwd())) not in sys.path:
@@ -14,6 +17,12 @@ from src.measure.measurers import ClassicMeasurer
 from src.wrappers import load_model
 
 if __name__ == '__main__':
+    exp_name = '1'
+    ckpt_dir = Path('checkpoints/seg_1/')
+    ds_dir = Path('datasets/prep/gazebo_vid/') / exp_name
+    video_dir = Path('videos') / ds_dir.parent.name / exp_name
+    video_dir.mkdir(exist_ok=True, parents=True)
+
     intrinsics_path = Path('params/intrinsics_1280.json')
     with open(intrinsics_path, 'r') as intrinsics_file:
         intrinsics = json.load(intrinsics_file)
@@ -24,7 +33,6 @@ if __name__ == '__main__':
         intrinsics=intrinsics,
     )
 
-    ckpt_dir = Path('checkpoints/seg_1/')
     ckpt_type = 'last.ckpt'
     model = load_model(
         ckpt_dir=ckpt_dir,
@@ -40,18 +48,61 @@ if __name__ == '__main__':
         lwh_max=(0.45, 0.32, 0.32),
         measurer=measurer,
         margin_px=50,
-        expand_px=10,
+        expand_px=0,
         fps=30,
+        store_detections=True,
     )
 
-    ds_dir = Path('datasets/prep/gazebo_vid/1/')
     hdf_paths_sorted = sorted(list(ds_dir.glob('*.hdf5')), key=lambda x: int(x.stem))
 
-    for idx, hdf_path in enumerate(hdf_paths_sorted):
+    box_annotator = sv.BoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
+    mask_annotator = sv.MaskAnnotator()
+    frames = []
+    for idx, hdf_path in enumerate(tqdm(hdf_paths_sorted)):
         with h5py.File(hdf_path) as data:
             colors = data['colors'][()]
             depth = data['depth'][()]
         rgbd_img = np.dstack((colors, depth))
         _res = cls.step(rgbd_img)
-        print(idx, _res)
-        print()
+
+        detections = cls.last_detections
+        labels = []
+        for det in detections:
+            det_idx = det[4]
+            if det_idx in _res:
+                r = _res[det_idx]
+                labels.append(
+                    f'#{det_idx}\n'
+                    f'l={r.length:.2f} w={r.width:.2f} h={r.height:.2f}\n'
+                    f'roundness={r.roundness:.2f}'
+                )
+            else:
+                labels.append(
+                    f'#{det_idx}\n'
+                    f'l=-1 w=-1 h=-1\n'
+                    f'roundness=-1'
+                )
+
+        annotated_frame = box_annotator.annotate(colors.copy(), detections=detections)
+        annotated_frame = label_annotator.annotate(annotated_frame, detections=detections, labels=labels)
+        annotated_frame = mask_annotator.annotate(annotated_frame, detections=detections)
+        frames.append(annotated_frame)
+
+    # Save video
+    for fps in (5, 15, 30):
+        video_path = video_dir / f'tracking_seg_{fps}fps.avi'
+        height, width = frames[0].shape[:2]
+
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        video = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
+
+        if not video.isOpened():
+            raise RuntimeError('VideoWriter failed to open')
+
+        for frame in frames:
+            bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            video.write(bgr_frame)
+
+        video.release()
+        print(f'Video saved to {video_path}')
